5e6bb36c8373cfa17069cb766a49f024ec1aa824
Sakshi Tiwari <154747002+Sakshitiwari5799@users.noreply.github.com>
2026-04-08 23:56:32 +0530

    Time: 1 ms (100%), Space: 43.3 MB (95.97%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -1,30 +1,23 @@
 class Solution {
-    public String longestPalindrome(String s) {
-        if (s == null || s.length() < 1) return "";
-
-        int start = 0, end = 0;
-
-        for (int i = 0; i < s.length(); i++) {
-            int len1 = expand(s, i, i);     // odd length
-            int len2 = expand(s, i, i + 1); // even length
-
-            int len = Math.max(len1, len2);
-
-            if (len > end - start) {
-                start = i - (len - 1) / 2;
-                end = i + len / 2;
-            }
-        }
-
-        return s.substring(start, end + 1);
+    int startIndex = 0, endIndex =0;
+      public  String longestPalindrome(String s) {
+       char [] chars = s.toCharArray();
+        backtrack(chars, 0);
+        return s.substring(startIndex, endIndex+1);
     }
-
-    private int expand(String s, int left, int right) {
-        while (left >= 0 && right < s.length() &&
-               s.charAt(left) == s.charAt(right)) {
-            left--;
-            right++;
+    private void backtrack(char[] chars, int i) {
+        int n = chars.length;
+        if(i> n-1) return;
+        int l = i, r = i;
+        while (r < n-1 && chars[r] == chars[r+1]) r++;
+        i = r;
+        while (l > 0 && r < n-1 && chars[l-1] == chars[r+1]){
+            l--; r++;
         }
-        return right - left - 1;
+        if(r-l > endIndex - startIndex){
+            startIndex = l;
+            endIndex = r;
+        }
+        backtrack(chars, ++i);
     }
 }
